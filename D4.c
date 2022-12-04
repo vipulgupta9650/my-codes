fa42b50d12c60403c78e99d26cf3d1a7d12c75c4
vipulgupta9650 <119300373+vipulgupta9650@users.noreply.github.com>
2022-12-04 15:47:31 +0530

    Add files via upload

---
+++ D4.c
@@ -0,0 +1,19 @@
+//print the sum of first n natural numbers using for loop
+//this is a very important question
+#include <stdio.h>
+int main() 
+{
+    int n;
+    printf("enter the value of n");
+    scanf("%d",&n);
+
+ int sum;
+   
+    for (int i=1;i<=n;i++)
+    {
+         sum = 0+i;
+        
+    }
+    printf("your sum is %d",sum);
+    return 0;
+}
